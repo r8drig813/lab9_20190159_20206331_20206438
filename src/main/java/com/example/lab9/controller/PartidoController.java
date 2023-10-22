a368f5417f2969f8d4c4c082b15f9d61b6803270
Sebastian <a20206438@pucp.edu.pe>
2023-10-22 11:41:40 -0500

    partidosvs

--- src/main/java/com/example/lab9/controller/PartidoController.java
+++ src/main/java/com/example/lab9/controller/PartidoController.java
@@ -2,6 +2,7 @@ package com.example.lab9.controller;
 
 import com.example.lab9.entity.Participante;
 import com.example.lab9.entity.Partido;
+import com.example.lab9.repository.ParticipanteRepository;
 import com.example.lab9.repository.PartidoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,19 +19,21 @@ public class PartidoController {
 
     final
     PartidoRepository partidoRepository;
+    final
+    ParticipanteRepository participanteRepository;
 
-
-    public PartidoController(PartidoRepository partidoRepository) {
+    public PartidoController(PartidoRepository partidoRepository, ParticipanteRepository participanteRepository) {
         this.partidoRepository = partidoRepository;
+        this.participanteRepository = participanteRepository;
     }
     //Listar Partidos
-    @GetMapping(value = {"/list", ""})
-    public List<Partido> listaParticipante() {
-        return partidoRepository.findAll();
-    }
+    @GetMapping(value = {"/getparticipantes"})
+    public List<Participante> listaParticipantes() {
+        return participanteRepository.findAll();
+    }git add ,
 
     // Obtener partidos
-    @GetMapping(value = "/{id}")
+    /*@GetMapping(value = "/{id}")
     public ResponseEntity<HashMap<String, Object>> buscarPartido(@PathVariable("id") String idStr) {
 
 
@@ -50,7 +53,7 @@ public class PartidoController {
         } catch (NumberFormatException e) {
             return ResponseEntity.badRequest().body(null);
         }
-    }
+    }*/
 
     //Registrar partido
     @PostMapping(value = {"", "/registro"})
@@ -67,6 +70,30 @@ public class PartidoController {
         responseJson.put("estado", "creado");
         return ResponseEntity.status(HttpStatus.CREATED).body(responseJson);
     }
+
+    //Obtener Participante
+    /*@GetMapping(value = "/getparticipantes1")
+    public ResponseEntity<HashMap<String, Object>> buscarPartipantes(@PathVariable("id") String idStr) {
+
+
+        try{
+            int id = Integer.parseInt(idStr);
+            //Optional<Partido> byId = partidoRepository.findById(id);
+            Optional<Participante> byId = participanteRepository.findById(id);
+
+            HashMap<String, Object> respuesta = new HashMap<>();
+
+            if (byId.isPresent()) {
+                respuesta.put("result", "ok");
+                respuesta.put("Participante", byId.get());
+            } else {
+                respuesta.put("result", "no existe");
+            }
+            return ResponseEntity.ok(respuesta);
+        } catch (NumberFormatException e) {
+            return ResponseEntity.badRequest().body(null);
+        }
+    }*/
 
 
 }
