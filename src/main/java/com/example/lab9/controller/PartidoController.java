e70f3f17846313d1d4ca59d5be6106d6081b3117
Sebastian <a20206438@pucp.edu.pe>
2023-10-22 09:27:06 -0500

    partido

--- src/main/java/com/example/lab9/controller/PartidoController.java
+++ src/main/java/com/example/lab9/controller/PartidoController.java
@@ -1,11 +1,72 @@
 package com.example.lab9.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.lab9.entity.Participante;
+import com.example.lab9.entity.Partido;
+import com.example.lab9.repository.PartidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/partido")
 public class PartidoController {
+
+    final
+    PartidoRepository partidoRepository;
+
+
+    public PartidoController(PartidoRepository partidoRepository) {
+        this.partidoRepository = partidoRepository;
+    }
+    //Listar Partidos
+    @GetMapping(value = {"/list", ""})
+    public List<Partido> listaParticipante() {
+        return partidoRepository.findAll();
+    }
+
+    // Obtener partidos
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<HashMap<String, Object>> buscarPartido(@PathVariable("id") String idStr) {
+
+
+        try {
+            int id = Integer.parseInt(idStr);
+            Optional<Partido> byId = partidoRepository.findById(id);
+
+            HashMap<String, Object> respuesta = new HashMap<>();
+
+            if (byId.isPresent()) {
+                respuesta.put("result", "ok");
+                respuesta.put("Partido", byId.get());
+            } else {
+                respuesta.put("result", "no existe");
+            }
+            return ResponseEntity.ok(respuesta);
+        } catch (NumberFormatException e) {
+            return ResponseEntity.badRequest().body(null);
+        }
+    }
+
+    //Registrar partido
+    @PostMapping(value = {"", "/registro"})
+    public ResponseEntity<HashMap<String, Object>> guardarPartido(
+            @RequestBody Partido partido,
+            @RequestParam(value = "fetchId", required = false) boolean fetchId) {
+
+        HashMap<String, Object> responseJson = new HashMap<>();
+
+        partidoRepository.save(partido);
+        if (fetchId) {
+            responseJson.put("id", partido.getId());
+        }
+        responseJson.put("estado", "creado");
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseJson);
+    }
 
 
 }
